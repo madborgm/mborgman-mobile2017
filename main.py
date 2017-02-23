a8be7965939c0c0328ce8500c0c87b1cb67f58c9
Maddie Borgmann <mborgman@andrew.cmu.edu>
2017-02-23 07:05:25 -0500

    content update

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+import json
+import urllib
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,13 +20,30 @@ app.config['DEBUG'] = True
 # the App Engine WSGI application server.
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
+@app.route('/')
+def index():
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
+    
+@app.route('/about')
+def about():
+    template = JINJA_ENVIRONMENT.get_template('templates/about.html')
+    return template.render()
+
+@app.route('/quality')
+def quality():
+    template = JINJA_ENVIRONMENT.get_template('templates/quality.html')
+    return template.render()
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+    return 'Sorry, Nothing at this URL.', 404
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
